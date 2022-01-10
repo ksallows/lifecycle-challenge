f0de44a3ebca8300943db9ffb678d3bb81ec0997
Katie Sallows <ktsallows@gmail.com>
2022-01-10 12:55:02 -0500

    challenge complete

--- src/Components/PokeFetch/PokeFetch.js
+++ src/Components/PokeFetch/PokeFetch.js
@@ -9,10 +9,18 @@ class PokeFetch extends Component {
       pokeInfo: '',
       pokeSprite: '',
       pokeName: '',
+      time: 10
     }
   }
 
   fetchPokemon() {
+    clearInterval(this.timer);
+    this.setState({ time: 10 })
+    //document.getElementById('timer').classList.add('visible')
+    //document.getElementById('timer').classList.remove('hidden')
+    document.getElementById('name').classList.add('hidden')
+    document.getElementById('name').classList.remove('visible')
+    document.getElementById('image').classList.add('dark')
     let min = Math.ceil(1);
     let max = Math.floor(152);
     let pokeNum = Math.floor(Math.random() * (max - min) + min);
@@ -27,16 +35,33 @@ class PokeFetch extends Component {
         })
       })
       .catch((err) => console.log(err))
+    this.timer = setInterval(() => {
+      if (this.state.time >= 1) this.setState((prevState) => ({ time: prevState.time - 1 }));
+    }, 1000);
+  }
+
+  componentDidUpdate() {
+    if (this.state.time <= 0) {
+      //document.getElementById('timer').classList.add('hidden')
+      //document.getElementById('timer').classList.remove('visible')
+      document.getElementById('name').classList.add('visible')
+      document.getElementById('name').classList.remove('hidden')
+      document.getElementById('image').classList.remove('dark')
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
   }
 
   render() {
     return (
       <div className={'wrapper'}>
         <button className={'start'} onClick={() => this.fetchPokemon()}>Start!</button>
-        <h1 className={'timer'} >Timer Display</h1>
+        <h1 id='timer' className={'timer'} >{this.state.time}</h1>
         <div className={'pokeWrap'}>
-          <img className={'pokeImg'} src={this.state.pokeSprite} />
-          <h1 className={'pokeName'}>{this.state.pokeName}</h1>
+          <img id='image' className={'pokeImg', 'dark'} src={this.state.pokeSprite} />
+          <h1 id='name' className={'pokeName', 'hidden'}>{this.state.pokeName}</h1>
         </div>
       </div>
     )
